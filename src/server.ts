42567e1c94d973e495ffb299551561c22ba2fc2c
Samuel Braga <samuel.braga@dtidigital.com.br>
2020-04-13 16:57:08 -0300

    Creating logic for creating appointments and applying software architecture concepts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+import express, { Router } from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello Word' }));
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('App is running!');
