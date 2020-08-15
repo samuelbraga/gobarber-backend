16d709cefe9145d5d8cfee5fd97ff94e6cb85744
Samuel Braga <samuel.fantini.braga@hotmail.com>
2020-08-15 12:53:05 -0300

    Finalizando testes para criacao de agendamentos

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -18,5 +18,24 @@ describe('CreateAppointment', () => {
     expect(appointment).toHaveProperty('date');
   });
 
-  // it('should not be able to create two appointments on the same date and time', () => {});
+  it('should not be able to create two appointments on the same date and time', async () => {
+    const fakeAppoitmentsRepository = new FakeAppoitmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppoitmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 0, 31, 10);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '123456',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '123456',
+      }),
+    ).rejects.toBeInstanceOf(Error);
+  });
 });
