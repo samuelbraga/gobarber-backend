fdd3bfd28fd6747793d69d7587db8879e57c8102
Samuel Braga <samuel.fantini.braga@hotmail.com>
2020-09-27 20:19:51 -0300

    Criando conexao com mongodb

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,27 @@
-module.exports = {
-  "type": "postgres",
-  "host": `${process.env.DB_HOST}`,
-  "port": `${process.env.DB_PORT}`,
-  "username": `${process.env.DB_USER}`,
-  "password": `${process.env.DB_PASS}`,
-  "database": `${process.env.DB_NAME}`,
-  "entities": ['./src/modules/**/infra/typeorm/entities/*.ts'],
-  "migrations": ['./src/shared/infra/typeorm/migrations/*.ts'],
-  "cli": {
-    "migrationsDir": './src/shared/infra/typeorm/migrations',
+module.exports = [
+  {
+    "name": "default",
+    "type": "postgres",
+    "host": `${process.env.DB_HOST}`,
+    "port": `${process.env.DB_PORT}`,
+    "username": `${process.env.DB_USER}`,
+    "password": `${process.env.DB_PASS}`,
+    "database": `${process.env.DB_NAME}`,
+    "entities": ['./src/modules/**/infra/typeorm/entities/*.ts'],
+    "migrations": ['./src/shared/infra/typeorm/migrations/*.ts'],
+    "cli": {
+      "migrationsDir": './src/shared/infra/typeorm/migrations',
+    },
   },
-}
+  {
+    "name": "mongo",
+    "type": "mongodb",
+    "host": `${process.env.MONGO_HOST}`,
+    "port": `${process.env.MONGO_PORT}`,
+    "username": `${process.env.MONGO_USER}`,
+    "password": `${process.env.MONGO_PASS}`,
+    "database": `${process.env.MONGO_SCHEMA}`,
+    "useUnifiedTopology": true,
+    "entities": ['./src/modules/**/infra/typeorm/schemas/*.ts'],
+  },
+]
